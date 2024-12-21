c46f75495ecb47f09ca983008bed76ab93b8b3d7
binusahal <sahal7648@gmail.com>
2024-12-21 22:45:32 +0300

    Exapression & Variable

---
+++ Expressions_.py
@@ -0,0 +1,50 @@
+# Numeric Expressions 
+
+# Operator     Operation 
+# +            Addition
+# -            Subtraction
+# *            Multiplication
+# /            Division
+# %            Modulus (Remainder)
+# **           Exponentiation
+# //           Floor Division (Truncates the result to an integer)
+# <            Less Than
+# >            Greater Than
+# <=           Less Than or Equal To
+# >=           Greater Than or Equal To
+# ==           Equal To
+# !=           Not Equal To
+# In the following code, we will use the operators to perform various mathematical operations.
+
+# xx =2 
+# yy = 3
+# print(xx + yy)  # Output: 5
+# print(xx - yy)  # Output: -1
+# print(xx * yy)  # Output: 6
+# print(xx / yy)  # Output: 0.6666666666666666
+# print(xx % yy)  # Output: 2
+# print(xx ** yy)  # Output: 8
+# print(xx // yy)  # Output: 0
+# print(xx < yy)  # Output: False
+# print(xx > yy)  # Output: False
+# print(xx <= yy)  # Output: False
+# print(xx >= yy)  # Output: True
+# print(xx == yy)  # Output: False
+# print(xx != yy)  # Output: True
+
+
+xx =2 
+yy = 3
+print(xx + yy)  # Output: 5
+print(xx - yy)  # Output: -1
+print(xx * yy)  # Output: 6
+print(xx / yy)  # Output: 0.6666666666666666
+print(xx % yy)  # Output: 2
+print(xx ** yy)  # Output: 8
+print(xx // yy)  # Output: 0
+print(xx < yy)  # Output: False
+print(xx > yy)  # Output: False
+print(xx <= yy)  # Output: False
+print(xx >= yy)  # Output: True
+print(xx == yy)  # Output: False
+print(xx != yy)  # Output: True
---
+++ Variables.py
@@ -0,0 +1,31 @@
+# Variables
+
+# Variables are containers for storing data values.
+# A variable is a named place in the memory  where a programmer can store data 
+# and later retrieve the data using the variable "name" .
+#  Programmer get to choose the name of the variables .  
+
+# Reserved Words in Python 
+
+# Python only has a few dozen reserved words: False , None , True , and , as , assert ,
+# async , await , break , class , continue , def , del , elif , else , except , finally
+# , for , from , global , if , import , in , is , lambda , nonlocal , not , or , pass ,
+# , raise , return , try , while , with , yield .
+
+
+x = 12.2
+y = 14 
+
+print("the value of X is:",x)
+print("the value of Y is:",y)
+
+
+# python Variable name Rule 
+
+# Must start with a letter or underscore_
+# Must Consist of letters , numbers and underscores
+# Case sensitive 
+
+# GOOD: spam  eggs   spam24   _speed
+# BAD: 1spam 2eggs spam24_  spam$eggs #sign var.12
+# different: Spam  eggs  SPAM24  _Speed SPAM
